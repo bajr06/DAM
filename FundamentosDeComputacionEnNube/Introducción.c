96eb6bb407efabed8ff0259664c8d044adbfc604
Bryan Andreu Jiménez Rojas <181849388+bajr06@users.noreply.github.com>
2024-10-23 23:57:22 +0200

    Actualización de .txt a .c

---
+++ FundamentosDeComputacionEnNube/Introducción.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+	printf("Fundamentos de computación en la nube\n\
+- Responsabilidad compartida\n\
+- Nube pública, privada, híbrida, múltiple y comunitaria.\n\
+- Alta disponibilidad.\n\
+- SLA.\n\
+- Escalada vertical y horizontal.\n\
+- Confiabilidad, Predicción, Rendimiento y Coste.\n\
+- Administración de y en la nube.\n");
+
+	return EXIT_SUCCESS;
+}
