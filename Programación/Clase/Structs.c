0939408a08f246ddf864cdbc20680842b261bd4d
bajr06 <181849388+bajr06@users.noreply.github.com.>
2024-11-11 21:07:59 +0100

    Prueba 2

--- Programación/Clase/Structs.c
+++ Programación/Clase/Structs.c
@@ -8,9 +8,9 @@
 */ 
 
 struct fecha{ //Declara el tipo de dato.
-	int día;
+	int dia;
 	int mes;
-	int año;
+	int anio;
 };
 
 struct cuenta{
@@ -30,9 +30,23 @@ int main(){
 	
 	printf("Número de cuenta: %d\n", usuario.num_cuenta);
 	printf("Tipo de cuenta: %c", usuario.tipo_cuenta);
-	printf("Día del último pago: %d\n", usuario.ultimopago.día);
+	printf("Día del último pago: %d\n", usuario.ultimopago.dia);
 	
-	//No funciona, revisar GitHub de Profesor.
+	/** Array de clientes */
+	cliente[0].ultimopago.dia = 24;
+	cliente[0].ultimopago.mes = 5;
+	cliente[0].ultimopago.anio = 1990;
+	cliente[0].num_cuenta = 12345;
+	cliente[0].tipo_cuenta = 'R';
+	//strcpy(cliente[0].nombre, "José");  // Usar strcpy para copiar cadenas en C
+	cliente[0].saldo = 586.30;
+
+	printf("%d\n",cliente[0].tipo_cuenta);
+
+	// Rellenamos el segundo cliente por pantalla:
+	printf("Introduce el número de cuenta: ");
+	scanf("%d",&cliente[1].num_cuenta);
+	printf("El número de cuenta es: %d\n",cliente[1].num_cuenta);
 
 	return EXIT_SUCCESS;
 }
