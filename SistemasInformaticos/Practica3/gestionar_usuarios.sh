bec99c798e519651fc285143f0ec4f2b52787ae8
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-03-06 10:48:33 +0100

    Fin de Script de Gestion de Usuarios - Sistemas Informaticos

--- SistemasInformaticos/Practica3/gestionar_usuarios.sh
+++ SistemasInformaticos/Practica3/gestionar_usuarios.sh
@@ -1,39 +1,52 @@
 #!/bin/bash
 
 t='	'
+afirmacion="Si"
+
+clear
 
 echo "Bienvenido al script supremo"
-echo "¿Cuál de las siguientes opciones quieres seleccionar?"
-echo "$t 1. Crear un nuevo usuario y asignar a un grupo ya existente"
-echo "$t 2. Eliminar usuario"
-echo "$t 3. Listar los usuarios existentes y ver su información"
 
-read opcion # Escanea la opcion que el administrador escoja por pantalla.
+while [ "$afirmacion" = "Si" ] || [ "$afirmacion" = "si" ] || [ "$afirmacion" = "sI" ] || [ "$afirmacio" = "SI" ];
+do
+	echo "¿Cuál de las siguientes opciones quieres seleccionar?"
+	echo "$t 1. Crear un nuevo usuario y asignar a un grupo ya existente."
+	echo "$t 2. Eliminar usuario."
+	echo "$t 3. Listar los usuarios existentes y ver su información."
+	echo "Después de usar cualquiera de las funciones dadas, puedes salir o volverlo a hacer."
 
-if [ $opcion -eq 1 ];
-then
-	echo "Escriba el nombre que quieras que tenga el usuario:"
-	read nuevo
+	read opcion
 
-	sudo adduser "$nuevo" # Ejecuta el comando para crear un nuevo usuario.
-
-	echo "Escriba el grupo al que quieres añadir al usuario:"
-	read grupo
+	if [ $opcion -eq 1 ];
+	then
+		echo "Escriba el nombre que quieras que tenga el usuario:"
+		read nuevo
+		
+		sudo adduser "$nuevo" # Ejecuta el comando para crear un nuevo usuario.
+		
+		echo "Escriba el grupo al que quieres añadir al usuario:"
+		read grupo
 	
-	sudo usermod -aG "$grupo" "$nuevo" # Lo añade a un grupo que ya exista con anterioridad
-elif [ $opcion -eq 2 ];
-then
-	echo "Escriba el nombre del usuario al cual quieras eliminar:"
-	read borrar
+		sudo usermod -aG "$grupo" "$nuevo" # Lo añade a un grupo que ya exista con anterioridad
+	elif [ $opcion -eq 2 ];
+	then
+		echo "Escriba el nombre del usuario al cual quieras eliminar:"
+		read borrar
 
-	sudo deluser "$borrar" # Borra al usuario (que se haya creado con anterioridad, o que ya exista).
-elif [ $opcion -eq 3 ];
-then
-	echo "Escriba el nombre del usuario al que le quieres ver la información:"
-	read nombre
+		sudo deluser "$borrar" # Borra al usuario (que se haya creado con anterioridad, o que ya exista).
+	elif [ $opcion -eq 3 ];
+	then
+		echo "Escriba el nombre del usuario al que le quieres ver la información:"
+		read nombre
 
-	groups "$nombre" # Muestra las carácteristicas del usuario.
-else
-	echo "No existe esa opción. Vuelva a ejecutar el script."
-fi
+		groups "$nombre" # Muestra las carácteristicas del usuario.
+	else
+		echo "No existe esa opción. Vuelva a ejecutar el script."
+	fi
+
+	echo "¿Quieres hacer otra opcion?"
+	read afirmacion
+	
+	clear
+done
 
