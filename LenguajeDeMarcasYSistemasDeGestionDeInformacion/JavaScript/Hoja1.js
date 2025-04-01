6293ba083329f6e337fb784270188d4b201adf71
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-04-01 13:21:35 +0200

    Arreglo Hoja1 JavaScript - Lenguaje de Marcas

--- LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Hoja1.js
+++ LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Hoja1.js
@@ -44,7 +44,6 @@ function Ejercicio6(){
 
 
 function Ejercicio7(){
-
 	let nuevo1 = parseInt(prompt("Escriba otra vez, nuevamente, un número:"));
 	let nuevo2 = parseInt(prompt("Escriba otra vez, nuevamemnte, otro número:"));
 	let nuevo3 = parseInt(prompt("Escriba otro de otro de otro número:"));
@@ -52,16 +51,39 @@ function Ejercicio7(){
 	if(nuevo1 > nuevo2){
 		if(nuevo1 > nuevo3){
 			if(nuevo2 > nuevo3){
-				console.log("El número " + nuevo1 + "es el número mayor <br>");
+				console.log("El número mayor es:" + nuevo1 + ", " + nuevo2 + ", " + nuevo3);
+			} else if(nuevo3 > nuevo2){
+				console.log("El número mayor es:" + nuevo1 + ", " + nuevo2 + ", " + nuevo3);
+			} else {
+				console.log("El número mayor es:" + nuevo1 + ", " + nuevo2 + " = " + nuevo3);
 			}
+		} else if(nuevo1 < nuevo3){
+			console.log("El número mayor es:" + nuevo3 + ", " + nuevo1 + ", " + nuevo2);
+		} else {
+			console.log("El número mayor es:" + nuevo1 + " = " + nuevo3 + ", " + nuevo2);
 		}
+	} else if(nuevo1 < nuevo2){
+		if(nuevo1 > nuevo3){
+			console.log("El orden:" + nuevo2 + " > " + nuevo1 + " > " + nuevo3);
+		} else if(nuevo3 > nuevo1){
+			console.log("El orden:" + nuevo2 + " > " + nuevo3 + ", " + nuevo1);
+		} else {
+			console.log("El orden:" + nuevo2 + ", " + nuevo3 + " = " + nuevo1);
+		}
+	} else {
+		console.log("El orden:" + nuevo2 + " = " + nuevo3 + " = " + nuevo3);
 	}
-	// Ver apuntes
-
 }
-*/
 
-// Ejercicio 8: num1 % 2 === 0;
+function Ejercicio8(){
+	let numero1 = parseInt(prompt("Escribe un número:"));
+
+	if(numero1 % 2 === 0){
+		console.log("Es divisible entre 2.");
+	} else {
+		console.log("No es divisible entre 2.");
+	}
+}
 
 function Ejercicio9(){
 	let frase = prompt("Escriba una frase:");
@@ -75,64 +97,68 @@ function Ejercicio9(){
 	}
 
 	document.writeln("El número de a es: " + contador + "<br>");
+}
 
-	// let cuenta = frase.split('a').lenght - 1; // Investigar sobre la funcion .split
-	
-	/*
+function Ejercicio9A(){
+	let frase = prompt("Escribe una frase:");
+	let cuenta = frase.split('a').lenght - 1; // Investigar sobre la funcion .split
+
+	document.writeln("El número de a es: " + cuenta + "<br>");
+}
+
+function Ejercicio9B(){
+	let frase = prompt("Escriba una frase:");
 	let letra='a';
+	let contador = 0:
 
-	for(let j of frase){
+	for(let j = 0; j < frase.length; j++){
 		if(frase.includes(letra)){
 			contador++;
 		}
 	}
-	*/
 
-	/*
-	for(let j = 0; i < frase.length; i++){
-		if(frase[i].includes(letra)){
-			contador++;
-		}
-	}
-	*/
+	alert(contardor);
+}
 
-	/*
-	let letra='a';
+function Ejercicio9C(){
+	let frase = "Hola, ¿Qué tal?";
+	let contador = 0;
 
 	for(let char of frase){
 		if(char.toLowerCase() === 'a'){
 			contador++;
 		}
 	}
-	*/
 	
+	document.writeln(contador "<br>");
 }
 
 function Ejercicio10(){
 	let frase = prompt("Escribe una frase cualquiera:");
 	let vocales = "aeiouAEIOU";
 	let vocales_encontradas = "";
+	let contador = 0;
 
 	for(let char of frase){
-		// if(!vocales_encontradas.includes(char))
 		if(vocales.includes(char)){
-			vocales_encontradas = vocales_encontradas + char;
-			// vocales_encontradas += char;
+			if(!vocales_encontradas.includes(char)){
+				vocales_encontradas += char;
+			}
+
+			contador++;
 		}
 	}
 
-	document.writeln("Vocales encontradas: " + vocales_encontradas + "<br>");
+	document.writeln("Vocales: " + vocales_encontradas + "; Número de vocales encontradas: " + contador + ". <br>");
 }
 
 function Ejercicio11(){
 	let frase = prompt("Escribe una frase cualquiera:");
 	let vocales = "aeiouAEIOU";
 	let vocales_encontradas = "";
-	let contador;
+	let contador = 0;
 
 	for(let char of frase){
-	
-	
 		if(vocales.includes(char)){
 			vocales_encontradas += char;
 		}
@@ -140,7 +166,7 @@ function Ejercicio11(){
 		contador++;
 	}
 
-	document.writeln("Vocales encontradas: " + vocales_encontradas + "<br>");
+	document.writeln("Vocales encontradas: " + vocales_encontradas + "; Total: " + contador + ". <br>");
 }
 
 function Ejercicio12(){
@@ -149,16 +175,16 @@ function Ejercicio12(){
 	let vocales_encontradas = {a:0, e:0, i:0, o:0, u:0};
 
 	for(let char of frase){
-		if(vocales.includes(char)){
-			vocales_encontradas[char,toLowerCase()];
+		if(vocales.includes(char.toLowerCase())){
+			vocales_encontradas[char,toLowerCase()]++;
 		}
 	}
 
-	document.writeln("Vocales: " + JSON.stringify(vocales_encontradas) + "<br>");
+	document.writeln("Vocales: " + JSON.stringify(vocales_encontradas) + ". <br>");
 }
 
 function Ejercicio14(){
-	let num = parseInt(prompt("Escriba un número:"));
+	let num = parseInt(prompt("Introduce un número: "));
 	let divisibles = [];
 	 
 	if(num % 2 === 0){
@@ -172,12 +198,23 @@ function Ejercicio14(){
 	}
 
 	if(divisibles.length > 0){
-		document.writeln(divisibles.join(", "));
+		document.writeln(divisibles.join(", ") ". <br>");
 	} else {
-		document.writeln("No es divisible.");
+		document.writeln("No es divisible. <br>");
 	}
 }
 
+function EjemploSplit(){
+	let frase="Hola que tal estas";
+	let palabras=frase.split(" ");
+
+	for(let i=0;i<palabras.length;i++){
+		document.writeln(palabras[i]+"<br>");
+	}
+
+	let veces=frase.split(" ").length-1;
+	document.writeln(veces);
+}
 
 // Función que llama a todas las funcoones y tiene el funcionamiento del main.
 function carga(){
@@ -187,9 +224,13 @@ function carga(){
 	Ejercicio4();
 	Ejercicio5();
 	Ejercicio6();
-	// Ejercicio7();
-	// Ejercicio8();
+	Ejercicio7();
+	Ejercicio8();
 	Ejercicio9();
+	Ejercicio9A();
+	Ejercicio9B();
+	EjemploSplit();
+	Ejercicio9C();
 	Ejercicio10();
 	Ejercicio11();
 	Ejercicio12();
