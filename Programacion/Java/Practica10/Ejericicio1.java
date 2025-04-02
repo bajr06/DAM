3a93ae22ef1ddb58e7159bdc878ecc99c4806f5e
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-04-02 14:02:18 +0200

    Avance Java - Programacion

---
+++ Programacion/Java/Practica10/Ejericicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio1{
+	static Scanner Pantalla = new Scanner(System-in);
+
+	public static void main(String[] args){
+		String Frase = "Hola, ¿Qué tal?";
+		int contador = 0;
+	}
+}
