3c7962d74d54ba37b04f8f34b77c99f5089ee80c
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-04-23 11:17:12 +0200

    Avance Eventos - Lenguaje de Marcas y Sistemas de Gestion de Informacion

--- LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Eventos.js
+++ LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Eventos.js
@@ -7,7 +7,14 @@ function cambiarFondo() {
 }
 
 function cambiarTexto() {
-	document.body.innerHTML = "<h1> Hola </h1> </h1>";
+	document.body.innerHTML = "<h1> Hola </h1>";
+}
+
+function mostrarDatos(event) {
+	event.preventDefault();
+	let nombre = document.getElementById("nombre").value;
+
+	document.getElementById("resultado").innerHTML = `valor: ${nombre}`;
 }
 
 
@@ -15,9 +22,16 @@ function carga() {
 	let boton = document.querySelector("button");
 	let boton2 = document.getElementById("boton2");
 	let cuadroTexto = document.getElementById("cuadroTexto");
-	
+	let boton3 = document.getElementById("boton3");
+	let zona = document.getElementById("zona");
+	let formulario = document.getElementById("formulario");
+
+	// Eventos
 	boton.addEventListener("click", cambiarFondo, false);
 	boton2.addEventListener("click", cambiarTexto, false);
-	cuadroTexto.addEvenetListener("keydown", (event) => ());
+	cuadroTexto.addEventListener("keydown", (event) => (document.getElementById("textoSalida").textContent += `${event.key}`));
+	boton3.addEventListener("click",(event) => console.log(event));
+	zona.addEventListener("mousemove", (event) => (document.getElementById("coordenadas").textContent = `Coordenadas : X = ${event.clientX} e Y = ${event.clientY}`));
+	formulario.addEventListener("submit", mostrarDatos);
 }
 window.addEventListener("DOMContentLoaded", carga, false);
