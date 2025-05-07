f6dc2e01f0834da5f95b692c01881cdc55628265
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-05-07 10:36:36 +0200

    Avance Ejercicio 3 - ÃLenguaje de Marcas y Sistemas de Gestion de Informacion

---
+++ LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Ejercicio3/Ejercicio3.js
@@ -0,0 +1,28 @@
+// Donde el indice es el número de cuadrados que tengo
+let barcos = [4, 3, 2, 1];
+// Tablero del usuario -> Matriz que guarda los barcos
+let tableroUsuario:
+
+// Posicion es la cadena que nos devuelve el id. Ejemplo id -> u01.
+function esBarco2(posicion1, posicion2, m) {
+	if(m[i][j] == m[][])
+}
+
+function guardarBarco(event) {
+	alert("Guardo el Barco");
+	let i = event.target.value[1];
+	let j = event.target.value[2];
+
+	if(i = event.target.id[0] === 'u') {
+		tableroUsuario[i][j] = 'x';
+	}
+	console.log("Posición guardada: " + event.target.name);
+}
+
+
+function principal() {
+	// Devuelve un array de elementos que tienen "celda".
+	let celdasTabla = document.querySelectorAll(".celda");
+	celdasTabla.forEach(celda => celda.addEventListener("keydown", guardarBarco));
+}
+window.addEventListener("DOMContentLoaded", principal, false);
