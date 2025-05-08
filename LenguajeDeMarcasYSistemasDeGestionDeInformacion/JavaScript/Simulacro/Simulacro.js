7ed5b6ae72eb86f9c722c1de73155caedb19b444
bajr06 <181849388+bajr06@users.noreply.github.com.>
2025-05-08 20:05:22 +0200

    Arreglos del Simulacro - Lenguaje de Marcas y Sistemas de Gestion de Informacion

--- LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Simulacro/Simulacro.js
+++ LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Simulacro/Simulacro.js
@@ -8,12 +8,15 @@ function traduccion(letra) {
 	let numero = 0;
 	let esta = false;
 
+	console.log(abecedario.length);
+
 	while(!esta) {
 		if(abecedario[numero] === letra) {
+			console.log(abecedario[numero]);
 			esta = true;
 		} else {
 			// En el caso de que no este entre las letras del abecedario.
-			if(abecedario.length === 26) {
+			if(numero === abecedario.length) {
 				numero =- 1;
 				esta = true;
 			} else {
@@ -29,7 +32,7 @@ function codificarTexto(event) {
 	let salida = document.getElementById("textoCodificado");
 	let letra = event.key;
 
-	salida.textContent += traduccion(letra) + "-";
+	salida.textContent += traduccion(letra) + " - ";
 }
 
 function random(numero) {
@@ -40,7 +43,7 @@ function generarDatos() {
 	letra = abecedario[random(26)];
 	coordenadaI = random(1);
 	coordenadaJ = random(1);
-	document.getElementById("letra").textContent = letra;
+	document.getElementById("letra").textContent = letraAdivinar;
 	document.getElementById("i").textContent = coordenadaI;
 	document.getElementById("j").textContent = coordenadaJ;
 }
@@ -51,6 +54,8 @@ function colocarCasillas(event) {
 	let j = event.target.name[1];
 	let letra = event.target.value;
 
+	console.log(i + ", " + j + " -> " + letra + " letra para adivinar: " + letraAdivinar);
+
 	if(i === coordenadaI && j === coordenadaJ && letraAdivinar === letra) {
 		document.getElementById(event.target.name).textContent = letra;
 	} else {
@@ -60,15 +65,18 @@ function colocarCasillas(event) {
 
 
 function principal() {
+	let intervalo;
+
 	// Ejercicio 1
 	let texto = document.getElementById("textoACodificar");
 	texto.addEventListener("keydown", codificarTexto);
 
 	// Ejercicio 2
-	intervalo = setInterval(generarDatos, 2000);
+	generarDatos();
+	intervalo = setInterval(generarDatos, 5000);
 
 	let letras = document.querySelectorAll(".letra");
-	letras.forEach(i => i.addEventListener("input", colocarCasilla))
+	letras.forEach(i => i.addEventListener("input", colocarCasillas))
 }
 
 window.addEventListener("DOMContentLoaded", principal);
