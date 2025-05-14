fd17d7180c3aca8be05f668657ae4c2545597c2d
bajr06 <bryanandreujimenezrojas@gmail.com>
2025-05-14 10:40:42 +0200

    Avances Ejercicio1 de Procedimientos - Base de Datos

--- BaseDeDatos/Procedimientos/Ejercicio1.sql
+++ BaseDeDatos/Procedimientos/Ejercicio1.sql
@@ -15,22 +15,58 @@ delimiter $$
 create procedure CambiarJefe(in CodEmpl int, in NueCodJefe int)
 begin
 	update Empleados set CodigoJefe = NuCodJefe where CodigoEmpleado = CodEmpl;
-
 end$$
 delimiter ;
 
 -- Ejercicio 3
-
+delimiter $$
+create procedure NombreEmpleadoYJefe(in CiudadEmpleadoYJefe varchar(30))
+begin
+	select Nombre as NombreJefe, NombreEmpleado from Empleados inner join (select Nombre as NombreEmpleado, CodigoJefe from Empleados inner join (select CodigoOficina from Oficinas where Ciudad = CiudadEmpleadoYJefe) T1 on Empleados.CodigoOficina = T1.CodigoOficina) T2 on Empleados.CodigoEmpleado = T2.CodigoJefe;
+end$$
+delimiter ;
 
 -- Ejercicio 4
-
+delimiter $$
+create procedure PedidosNoCancelados(in NombCliente varchar(50))
+begin
+	select CodigoPedido, Estado from Pedidos inner join (select CodigoCliente from Clientes where NombreCliente = NombCliente) T1 on Pedidos.CodigoCliente = T1.CodigoCliente where Estado != "Rechazado";
+end$$
+delimiter ;
 
 -- Ejercicio 5
-
+delimiter $$
+create procedure PrecioProducto(in NombreProducto varchar(70))
+begin
+	select Nombre, PrecioVenta, PrecioProveedor from Productos where Nombre = NombreProducto;
+end$$
+delimiter ;
 
 -- Ejercicio 6
+/*
+delimiter $$
+create procedure NuevoPedido(in Producto varchar(70), in CantidadProducto int)
+begin
+	declare Codigo int;
+	declare Codigo2 varchar(15);
+	
+	set Codigo = (select count(*) from DetallePedidos) + 1;
+	set Codigo2 = (select CodigoProducto from Productos where Nombre = Producto limit 1);
+
+	insert into DetallePedidos values(Codigo, Codigo2, CantidadProducto, NULL, NULL);
+end$$
+delimiter ;
+*/
+
+delimiter $$
+create procedure NuevoPedido(in Id_Cliente int, Id_Producto varchar(15))
+begin
+	declare Codigo1 int;
 
 
+	set Codigo = (select count(*) from Pedidos) + 1;
+end$$
+delimiter ;
 -- Ejercicio 7
 
 
@@ -42,3 +78,7 @@ delimiter ;
 call NuevoEmpleado("Bryan", "Jiménez", "Rojas", 3500, "bryanandreujimenezrojas@gmail.com", "ECU-ES", 1, "Programador");
 
 call CambiarJefe(50, 8);
+
+call NombreEmpleadoYJefe("Madrid");
+
+call PedidosNoCancelados2("FLORES S.L.");
