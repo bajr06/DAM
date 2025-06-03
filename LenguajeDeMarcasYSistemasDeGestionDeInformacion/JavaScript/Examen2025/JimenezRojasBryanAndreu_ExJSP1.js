d6153f0ee03dc41baecf71518c9de213c077d5ae
bajr06 <181849388+bajr06@users.noreply.github.com.>
2025-06-03 20:00:07 +0200

    Examen 1 de JavaScript terminado - Lenguaje de Marcas y Sistemas de Gestion de Informacion

--- LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Examen2025/JimenezRojasBryanAndreu_ExJSP1.js
+++ LenguajeDeMarcasYSistemasDeGestionDeInformacion/JavaScript/Examen2025/JimenezRojasBryanAndreu_ExJSP1.js
@@ -1,4 +1,14 @@
-	let oc = aleatorio(2) + 10;
+let oc = aleatorio(2) + 10;
+const azul = `rgb(0, 0, 255)`;
+const amarillo = `rgb(255, 255, 0)`;
+const verde = `rgb(0, 255, 0)`;
+const naranja = `rgb(255, 102, 0)`;
+const rosa = `rgb(255, 153, 204)`;
+const morado = `rgb(153, 0, 204)`;
+const marron = `rgb(102, 51, 51)`;
+const cyan = `rgb(0, 255, 255)`;
+const turquesa = `rgb(255, 0, 255)`;
+const gris = `rgb(128, 128, 128)`;
 
 function comprobarMatricula(matricula)  {
 	const regex = /^[A-Z]{3}[0-9]{4}$/; 
@@ -63,6 +73,65 @@ function ejercicio3(event) {
 	}
 }
 
+function cambiarColor(valor) {
+	let cambiarColor = document.getElementById("cambiarColor");
+
+	switch(valor) {
+		case 0:
+			cambiarColor.style.background = azul;
+			break;
+		case 1:
+			cambiarColor.style.background = amarillo;
+			break;
+		case 2:
+			cambiarColor.style.background = verde;
+			break;
+		case 3:
+			cambiarColor.style.background = naranja;
+			break;
+		case 4:
+			cambiarColor.style.background = rosa;
+			break;
+		case 5:
+			cambiarColor.style.background = morado;
+			break;
+		case 6:
+			cambiarColor.style.background = marron;
+			break;
+		case 7:
+			cambiarColor.style.background = cyan;
+			break;
+		case 8:
+			cambiarColor.style.background = turquesa;
+			break;
+		case 9:
+			cambiarColor.style.background = gris;
+	}
+}
+
+function ejercicio4() {
+	let cuenta = 0;
+	clearInterval(cuenta);
+
+	let valor = 0;
+	let valorTotal = 0;
+	let contador = document.getElementById("contador");
+
+	cuenta = setInterval(() => {
+		contador.textContent = valorTotal++;
+		console.log(valor);
+		if(valor >= 0 && valor <= 8) {
+			cambiarColor(valor);
+			valor++;
+		} else if(valor == 9){
+			cambiarColor(valor);
+			valor -= 9;
+		}
+	}, 1000);
+
+}
+
+
 function carga() {
 	// Ejercicio 1
 	let boton1 = document.getElementById("boton1");
@@ -75,8 +144,13 @@ function carga() {
 	boton2.addEventListener("click", ejercicio2);
 
 	// Ejercicio 3
-	let botones = document.querySelectorAll(".seleccionar");
+	let botones3 = document.querySelectorAll(".seleccionar");
 
-	botones.forEach(opciones => opciones.addEventListener("click", ejercicio3));
+	botones3.forEach(opciones => opciones.addEventListener("click", ejercicio3));
+
+	// Ejercicio 4
+	let boton4 = document.getElementById("boton4");
+
+	boton4.addEventListener("click", ejercicio4);
 }
 window.addEventListener("DOMContentLoaded", carga);
